33aae6dd56cca9fb97f18a04129863f329971162
Nidvar <djayshine87@hotmail.com>
2020-09-19 15:16:35 +1000

    postList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import PostList from './components/PostList'
 
 function App() {
   return (
     <div className="App">
       <h1>Redux Thunk</h1>
+      <PostList />
     </div>
   );
 }
